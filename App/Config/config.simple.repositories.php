b233dd44f860bc9eca176886a29272c358f62c8a
jsavell <jsavell@library.tamu.edu>
2017-05-16 16:32:17 -0500

    update configuration class name

--- App/Config/config.simple.repositories.php
+++ App/Config/config.simple.repositories.php
@@ -4,5 +4,5 @@ use Core\Classes\Configuration as CoreConfiguration;
 
 define('SIMPLE_REPOSITORY_KEY', 'simpleRepositories');
 
-$GLOBALS[SIMPLE_REPOSITORY_KEY] = array("SimpleRepoExample"=>new CoreConfiguration\SimpleRepositoryConfiguration('simple_repo_ex','id','name',null,array('name','description')));
+$GLOBALS[SIMPLE_REPOSITORY_KEY] = array("SimpleRepoExample"=>new CoreConfiguration\SimpleDatabaseRepositoryConfiguration('simple_repo_ex','id','name',null,array('name','description')));
 ?>
