0a5baeb1418e237415a7261a4f1581d4c41b9866
jsavell <jsavell@library.tamu.edu>
2017-09-27 09:54:53 -0500

    add a directory scan/upload demo page

--- App/Lib/functions.php
+++ App/Lib/functions.php
@@ -6,4 +6,15 @@
 */
 
 require_once PATH_CORE_LIB."functions.php";
+
+function buildUploadForm($baseUrl) {
+	$html = '<form class="do-upload form-inline" name="upload" method="POST" action="'.$baseUrl.'">';
+	$html .= '	<input type="hidden" name="action" value="upload" />
+				<input class="do-file-gloss" type="hidden" name="fileGloss" value="" />
+				<img class="do-file-preview" src="" />
+				<input class="inline-block" type="file" name="file_input" />
+				<input class="btn btn-default" type="submit" name="submitupload" value="Upload File" />
+			</form>';
+	return $html;
+}
 ?>
