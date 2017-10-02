4780a8c6dd2b7c8b7f3506acb81eee9927386a60
jsavell <jsavell@library.tamu.edu>
2017-10-02 15:37:22 -0500

    implement and demo form validation for html theme

--- App/Views/html/widgets.edit.view.php
+++ App/Views/html/widgets.edit.view.php
@@ -1,13 +1,21 @@
 <?php
 $widget = $parameters['widget'];
-echo '<form class="do-submit" name="editwidget" method="POST" action="'.$app_http.'">
+echo '<form class="do-submit-validate" name="editwidget" method="POST" action="'.$app_http.'">
 			<input type="hidden" name="action" value="update" />
 			<input type="hidden" name="id" value="'.$widget['id'].'" />
 			<div class="column column-half">
-				<label for="widget[name]">Name</label>
-				<input type="text" name="widget[name]" value="'.$widget['name'].'" />
-				<label for="widget[description]">Description</label>
-				<textarea name="widget[description]">'.$widget['description'].'</textarea>
+				<div class="form-group do-validate">
+					<label for="widget[name]">Name</label>
+					<input type="text" name="widget[name]" value="'.$widget['name'].'" />
+				</div>
+				<div data-validator="validateNumeric" class="form-group do-validate">
+					<label for="widget[part_count]">Count</label>
+					<input type="number" name="widget[part_count]" value="'.$widget['part_count'].'" />
+				</div>
+				<div class="form-group">
+					<label for="widget[description]">Description</label>
+					<textarea name="widget[description]">'.$widget['description'].'</textarea>
+				</div>
 			</div>
 			<input type="submit" name="submitwidget" value="Update Widget" />
 		</form>';
