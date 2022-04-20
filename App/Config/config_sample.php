a5e9c8e3abb446be0e0cf78068d576d48e86aae2
Jason Savell <jsavell@library.tamu.edu>
2022-04-20 16:27:59 -0500

    refactor config filenames

--- App/Config/config_sample.php
+++ App/Config/config_sample.php
@@ -10,7 +10,6 @@ define('PATH_APP', PATH_ROOT.APP_DIRECTORY.'/');
 
 define('WITH_COMPOSER',true);
 
-
 if (WITH_COMPOSER) {
 	define('VENDOR_DIRECTORY', 'vendor');
 	define('PATH_VENDOR', PATH_APP.VENDOR_DIRECTORY.'/');
@@ -23,25 +22,26 @@ if (WITH_COMPOSER) {
 //Add port if non-standard
 define('PATH_HTTP', "http://localhost/".APP_DIRECTORY."/site/");
 
-define("SESSION_SCOPE",APP_DIRECTORY);
+define("SESSION_SCOPE", APP_DIRECTORY);
 
 //defines the primary namespaces.
 //the autoloader defined in Pipit/Lib/functions.php depends on these values to find and load Class and Interface files
 //Individual files will need to have their namespaces updated to match if these are changed.
-define("NAMESPACE_CORE","Pipit\\");
-define("NAMESPACE_APP","App\\");
+define("NAMESPACE_CORE", "Pipit\\");
+define("NAMESPACE_APP", "App\\");
 
-//server paths
-//These don't need to be touched unless you're changing the location of the respective directories
+/**
+ * Server Paths
+ *
+ * These don't need to be touched unless you're changing the location of the respective directories
+ */
+
+//The location of the module ini files
 define('PATH_CONFIG', PATH_APP.str_replace('\\', '/', NAMESPACE_APP)."Config/");
 define('PATH_LIB', PATH_APP.str_replace('\\', '/', NAMESPACE_APP)."Lib/");
 define('PATH_CORE_LIB', PATH_CORE.str_replace('\\', '/', NAMESPACE_CORE)."Lib/");
 define('PATH_CONTROLLERS', PATH_APP.str_replace('\\', '/', NAMESPACE_APP)."Controllers/");
 define('PATH_VIEWS', PATH_APP.str_replace('\\', '/', NAMESPACE_APP)."Views/");
-
-//Sets where user uploaded files will be placed
-//Core\Classes\Helpers\FileManager is dependent on this configuration
-define('UPLOAD_PATH',PATH_ROOT.'uploads/');
 
 //web paths
 //These don't need to be touched unless you want to put your site assets somewhere else
@@ -51,44 +51,20 @@ define('PATH_CSS', PATH_RESOURCES."css/");
 define('PATH_JS', PATH_RESOURCES."js/");
 define('PATH_IMAGES', PATH_RESOURCES."images/");
 
-//Setting this to true triggers an attempt to log in to a CAS server using the following CAS configuration
+/*
+* To enable CAS:
+* - Set USECAS to true
+* - Configure user.cas.config.ini
+* - Define CAS_USER_REPO
+*/
 define('USECAS', false);
 
-define('CAS_URLS_BASE', NULL);
-define('CAS_LOGIN', "cas/login");
-define('CAS_CHECK', "cas/serviceValidate");
-define('CAS_LOGOUT', "cas/logout");
-define('CAS_URLS_LOGIN', CAS_URLS_BASE.CAS_LOGIN."?service=".PATH_HTTP."&renew=true");
-define('CAS_URLS_CHECK', CAS_URLS_BASE.CAS_CHECK."?service=".PATH_HTTP."&renew=true");
-define('CAS_URLS_LOGOUT', CAS_URLS_BASE.CAS_LOGOUT."?service=".PATH_HTTP."user.php?action=logout");
-define('CAS_REDIRECT_URL', PATH_HTTP);
+//When set, CoreSite will try to use NAMESPACE_APP\Classes\Data\USER_CLASS as the globalUser
+//Required for CAS use: A DataRepository representing the app's Users (requires existence of 'username' and 'iscas' fields)
+//define('CAS_USER_REPO','Users');
 
-//ldap config
-define('LDAP_URL', NULL);
-define('LDAP_PORT', NULL);
-define('LDAP_USER', NULL);
-define('LDAP_PASSWORD', NULL);
-define('LDAP_BASE_DN', NULL);
-define('LDAP_SEARCH_FILTER', NULL);
-//ToDo upgrade to PHP 5.6+ to allow for Array constants
-//const LDAP_SEARCH_ATTRIBUTES = array();
-//const LDAP_USER_MAP = array();
-define('LDAP_INACTIVE_USER_KEY', NULL);
-define('LDAP_USERNAME_KEY', NULL);
-
-//db config
-define('DB_USER', '');
-define('DB_PASSWORD', '');
-define('DB_HOST', '');
-define('DB_DATABASE', 'pipit_seed');
-define("DB_DSN", 'mysql:host='.DB_HOST.';dbname='.DB_DATABASE);
-
-//If switching to a non-MySQL flavor of SQL, expect to troubleshoot
-//define("DB_DSN", 'sqlsrv:Server='.DB_HOST.';Database='.DB_DATABASE);
-//define("DB_DSN", 'dblib:host='.DB_HOST.';dbname='.DB_DATABASE);
-
-//debug mode for PDO database queries
-define('DB_DEBUG', false);
+//Optionally define a custom implementation of \Pipit\Interfaces\User to be used for the GlobalUser
+//define('USER_CLASS',NAMESPACE_APP.'\\Classes\Data\\AppUserCAS');
 
 //To override the default CoreLogger, uncomment this and add functionality to the App\\Classes\\Logger
 //You could also extend the Core or App Loggers or implement the Logger interface directly. Just define the resulting namespaced class, here:
@@ -109,9 +85,9 @@ define("LOG_LEVEL",3);
 //Define the theme folder HTMLViewRenderer based ViewRenderers should use (default options are 'bootstrap' and 'html')
 define('ACTIVE_THEME','bootstrap');
 
-//When set, CoreSite will try to use NAMESPACE_APP\Classes\Data\USER_CLASS as the globalUser
-//define('USER_CLASS','User');
-
+/**
+ * The page access levels (use when defining pages in the site.pages config file)
+ */
 define('SECURITY_PUBLIC',-1);
 define('SECURITY_USER',0);
 define('SECURITY_ADMIN',1);
